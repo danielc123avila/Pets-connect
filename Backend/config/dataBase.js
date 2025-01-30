a98358d71f6eb67dc0c3936fd5ea69b845e6443d
daniel_colina <danielc123avila@icloud.com>
2025-01-30 09:29:22 -0500

    base de datos actualizada

--- Backend/config/dataBase.js
+++ Backend/config/dataBase.js
@@ -5,14 +5,12 @@ const mongoUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoUri);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("Error connecting to MongoDB", error);
     process.exit(1);
   }
 };
+
 export default connectDB;
