99d478e200abb7ad34e01e1c624101d54aad9a28
Juan Diego Higinio Aranzazu <juanhiginio06@gmail.com>
2025-02-14 09:47:41 -0500

    Landing page

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PieDePaginaComponent } from './componentes/pie-de-pagina/pie-de-pagina.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EncabezadoComponent, PieDePaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
